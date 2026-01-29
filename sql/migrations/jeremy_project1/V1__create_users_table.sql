a3475f8c34c05f29332b76a5003b4e400a07e7b1
Jeremy McWilliams <jeremym@lclark.edu>
2026-01-29 20:33:43 +0000

    first migration successful

---
+++ sql/migrations/jeremy_project1/V1__create_users_table.sql
@@ -0,0 +1,7 @@
+-- V1__create_users_table.sql
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
